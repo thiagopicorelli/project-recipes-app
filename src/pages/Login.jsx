ecf8d7fbd4161bf880221f393b4a530dfad23b0c
Virkkunen <vrkknn@vrkknn.net>
2023-01-25 17:02:12 -0300

    (req 04) validacao input email e senha

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,38 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Form, Row } from 'react-bootstrap';
 
 function Login() {
-  const email = useRef('');
-  const password = useRef('');
+  // const email = useRef('');
+  // const password = useRef('');
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
+  const [btnDisabled, setBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    const validateBtn = () => {
+      const MIN_PASSWD = 6;
+      const emailRegex = /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm;
+
+      const emailValid = emailRegex.test(loginData.email);
+      const passwdValid = loginData.password.length > MIN_PASSWD;
+      return !(emailValid && passwdValid);
+    };
+
+    setBtnDisabled(validateBtn());
+  }, [loginData]);
+
   // useEffect(() => { console.log(test.current.value); }, [test]);
   const handleSubmit = (e) => {
     e.preventDefault();
+  };
+
+  const handleChange = ({ target: { value, name } }) => {
+    setLoginData({
+      ...loginData,
+      [name]: value,
+    });
   };
 
   return (
@@ -20,22 +46,27 @@ function Login() {
             <Form.Control
               type="email"
               placeholder="Email"
-              ref={ email }
               data-testid="email-input"
+              value={ loginData.email }
+              name="email"
+              onChange={ handleChange }
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Control
               type="password"
               placeholder="Password"
-              ref={ password }
               data-testid="password-input"
+              value={ loginData.password }
+              name="password"
+              onChange={ handleChange }
             />
           </Form.Group>
           <Button
             variant="primary"
             type="submit"
             data-testid="login-submit-btn"
+            disabled={ btnDisabled }
           >
             Enter
           </Button>
