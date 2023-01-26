b7a86c626455721c5ed62cc287f0f7a2bb434ea8
Virkkunen <vrkknn@vrkknn.net>
2023-01-26 18:13:29 -0300

    (profile) testes simplificados

--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -4,28 +4,19 @@ import userEvent from '@testing-library/user-event';
 import App from '../App';
 import { renderWithRouter } from './helpers/renderWithRouter';
 
+const emailObj = { email: 'email@emaisl.com' };
+const initialEntries = ['/profile'];
+
 describe('Testes da tela Profile', () => {
   beforeEach(() => {
-    jest.spyOn(Storage.prototype, 'getItem');
+    window.localStorage.setItem('user', JSON.stringify(emailObj));
   });
 
-  xtest('Testa se o localStorage é acessado', () => {
-    expect(localStorage.getItem).toHaveBeenCalled();
-  });
+  afterEach(() => window.localStorage.clear());
 
   test('Testa se os elementos corretos estão na página', () => {
-    renderWithRouter(<App />);
-
-    const loginEmailEl = screen.getByRole('textbox');
-    const loginPasswdEl = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByRole('button', { name: /enter/i });
-
-    userEvent.type(loginEmailEl, 'email@email.com');
-    userEvent.type(loginPasswdEl, '12345678');
-    userEvent.click(loginBtn);
-
-    const profileEl = screen.getByAltText(/ícone de perfil/i);
-    userEvent.click(profileEl);
+    renderWithRouter(<App />, { initialEntries });
+    // JSON.parse(localStorage.getItem('user'));
 
     const emailEl = screen.getByRole('heading', { level: 4 });
     const doneBtn = screen.getByRole('button', { name: /done recipes/i });
@@ -33,29 +24,21 @@ describe('Testes da tela Profile', () => {
     const logoutBtn = screen.getByRole('button', { name: /logout/i });
 
     expect(emailEl).toBeInTheDocument();
+    expect(emailEl).toHaveTextContent('email@emaisl.com');
     expect(doneBtn).toBeInTheDocument();
     expect(favoriteBtn).toBeInTheDocument();
     expect(logoutBtn).toBeInTheDocument();
   });
 
   test('Se os botões levam para as páginas corretas', () => {
-    const { history } = renderWithRouter(<App />);
-
-    const loginEmailEl = screen.getByRole('textbox');
-    const loginPasswdEl = screen.getByPlaceholderText(/password/i);
-    const loginBtn = screen.getByRole('button', { name: /enter/i });
-
-    userEvent.type(loginEmailEl, 'email@email.com');
-    userEvent.type(loginPasswdEl, '12345678');
-    userEvent.click(loginBtn);
-
-    const profileEl = screen.getByAltText(/ícone de perfil/i);
-    userEvent.click(profileEl);
+    const { history } = renderWithRouter(<App />, { initialEntries });
 
     const doneBtn = screen.getByRole('button', { name: /done recipes/i });
 
     userEvent.click(doneBtn);
     expect(history.location.pathname).toBe('/done-recipes');
+
+    const profileEl = screen.getByAltText(/ícone de perfil/i);
 
     userEvent.click(profileEl);
     const favoriteBtn = screen.getByRole('button', { name: /favorite recipes/i });
