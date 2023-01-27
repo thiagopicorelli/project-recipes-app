fc6b6f0bd92c19bd7dcfc9ccf4637c8fe97c273e
Virkkunen <vrkknn@vrkknn.net>
2023-01-27 15:11:41 -0300

    componente de card para doneRecipes

---
+++ src/components/CardDoneRecipe.jsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Stack } from 'react-bootstrap';
+import getDoneRecipes from '../helpers/getDoneRecipes';
+
+export default function CardDoneRecipe() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    setDoneRecipes(getDoneRecipes());
+  }, []);
+
+  return (
+    <Stack
+      direction="vertical"
+      gap={ 3 }
+    >
+      { doneRecipes && doneRecipes.map((recipe, index) => (
+        <Card key={ recipe.id }>
+          <Card.Img
+            variant="top"
+            src={ recipe.image }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <Card.Body>
+            <Card.Title
+              data-testid={ `${index}-horizontal-name` }
+            >
+              { recipe.name }
+            </Card.Title>
+            <Card.Subtitle
+              className="mb-2 text-muted"
+              data-testid={ `${index}-horizontal-top-text` }
+            >
+              { recipe.category }
+            </Card.Subtitle>
+            <Card.Subtitle
+              className="mb-2 text-muted"
+              data-testid={ `${index}-horizontal-done-date` }
+            >
+              { recipe.doneDate }
+            </Card.Subtitle>
+            <Stack
+              direction="horizontal"
+              gap={ 1 }
+              className="mb-2"
+            >
+              {recipe.tags.map((tag) => (
+                <span
+                  data-testid={ `${index}-${tag}-horizontal-tag` }
+                  key={ tag }
+                >
+                  { tag }
+                </span>
+              ))}
+            </Stack>
+
+            <Button
+              variant="info"
+              size="sm"
+              data-testid={ `${index}-horizontal-share-btn` }
+            >
+              Share
+            </Button>
+          </Card.Body>
+        </Card>
+      ))}
+
+    </Stack>
+  );
+}
--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,14 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { Button, ButtonGroup, Card, Container, Row, Stack } from 'react-bootstrap';
-import getDoneRecipes from '../helpers/getDoneRecipes';
+import React from 'react';
+import { Button, ButtonGroup, Container, Row } from 'react-bootstrap';
+import CardDoneRecipe from '../components/CardDoneRecipe';
 
 function DoneRecipes() {
-  const [doneRecipes, setDoneRecipes] = useState([]);
-
-  useEffect(() => {
-    setDoneRecipes(getDoneRecipes());
-  }, []);
-
   return (
     <Container>
       <Row>
@@ -36,63 +30,7 @@ function DoneRecipes() {
           </Button>
         </ButtonGroup>
       </Row>
-      <Stack
-        direction="vertical"
-        gap={ 3 }
-      >
-        { doneRecipes && doneRecipes.map((recipe, index) => (
-          <Card key={ recipe.id }>
-            <Card.Img
-              variant="top"
-              src={ recipe.image }
-              data-testid={ `${index}-horizontal-image` }
-            />
-            <Card.Body>
-              <Card.Title
-                data-testid={ `${index}-horizontal-name` }
-              >
-                { recipe.name }
-              </Card.Title>
-              <Card.Subtitle
-                className="mb-2 text-muted"
-                data-testid={ `${index}-horizontal-top-text` }
-              >
-                { recipe.category }
-              </Card.Subtitle>
-              <Card.Subtitle
-                className="mb-2 text-muted"
-                data-testid={ `${index}-horizontal-done-date` }
-              >
-                { recipe.doneDate }
-              </Card.Subtitle>
-              <Stack
-                direction="horizontal"
-                gap={ 1 }
-                className="mb-2"
-              >
-                {recipe.tags.map((tag) => (
-                  <span
-                    data-testid={ `${index}-${tag}-horizontal-tag` }
-                    key={ tag }
-                  >
-                    { tag }
-                  </span>
-                ))}
-              </Stack>
-
-              <Button
-                variant="info"
-                size="sm"
-                data-testid={ `${index}-horizontal-share-btn` }
-              >
-                Share
-              </Button>
-            </Card.Body>
-          </Card>
-        ))}
-
-      </Stack>
-
+      <CardDoneRecipe />
     </Container>
   );
 }
